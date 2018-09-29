3843be0659b5674be7e3afea28c8829d0f40112f
aimontano <aimorales.montano@gmail.com>
2018-09-29 16:27:47 -0400

    Got song search functionallity working

--- liri.js
+++ liri.js
@@ -4,15 +4,41 @@ const Spotify = require('node-spotify-api');
 
 var spotify = new Spotify(keys.spotify);
 
-spotify.search({
-	type: "track",
-	query: "You Say",
-	limit: 2
-}, (err, data) => {
-	if(err)
-		console.log(err)
+let action = process.argv[2];
+let input = process.argv[3];
 
-	let artists = data.tracks.items[0].album.artists[0].name
+let artist = input;
+let searchQuery = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
 
-	console.log(artists);
-})
+// functions takes a song string
+// logs - artist name, song name, preview link
+const getSpotifySong = song => {
+	spotify.search({
+		type: "track",
+		query: song,
+		limit: 20
+	}, (err, data) => {
+		if(err)
+			console.log(err)
+
+		for(let i = 0; i < data.tracks.items.length; i++){
+
+			let artists = data.tracks.items[i].album.artists[0].name
+			let songName = data.tracks.items[i].album.name;
+			let preview = data.tracks.items[i].preview_url;
+			console.log("*************************************")
+			console.log("Result: #" +  (i + 1));
+			console.log("Artist: " + artists);
+			console.log("Song name: " + songName);
+			console.log("Preview url: " + preview);
+		}
+	});
+}
+
+
+
+switch (action) {
+	case "spotify-this-song":
+		getSpotifySong(input);
+		break;
+}
