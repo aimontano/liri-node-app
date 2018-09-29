ecf835f85a54d9cfb91f68e50c7d785c78680927
aimontano <aimorales.montano@gmail.com>
2018-09-29 17:22:11 -0400

    Added functionallity to get venues information

--- liri.js
+++ liri.js
@@ -1,6 +1,7 @@
 const dotenv = require("dotenv").config();
 const keys = require('./keys');
 const Spotify = require('node-spotify-api');
+const axios = require('axios');
 
 var spotify = new Spotify(keys.spotify);
 
@@ -8,7 +9,23 @@ let action = process.argv[2];
 let input = process.argv[3];
 
 let artist = input;
-let searchQuery = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+const getVenueInfo = artist => {
+	let searchQuery = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+	axios.get(searchQuery).then(response => {
+		let venues = [];
+
+		for(let i = 0; i < response.data.length; i++) {
+			console.log("\n***************************************")
+			console.log("Result #" + (i + 1))
+			console.log("Venue Information:")
+			console.log("--> name: " + response.data[i].venue.name);
+			console.log("--> location: " + response.data[i].venue.city + ", " + response.data[i].venue.region);
+			console.log("--> time: " + response.data[i].datetime);
+		}
+	});
+}
 
 // functions takes a song string
 // logs - artist name, song name, preview link
@@ -26,11 +43,11 @@ const getSpotifySong = song => {
 			let artists = data.tracks.items[i].album.artists[0].name
 			let songName = data.tracks.items[i].album.name;
 			let preview = data.tracks.items[i].preview_url;
-			console.log("*************************************")
+			console.log("\n*************************************")
 			console.log("Result: #" +  (i + 1));
-			console.log("Artist: " + artists);
-			console.log("Song name: " + songName);
-			console.log("Preview url: " + preview);
+			console.log("--> Artist: " + artists);
+			console.log("--> Song name: " + songName);
+			console.log("--> Preview url: " + preview);
 		}
 	});
 }
@@ -40,5 +57,8 @@ const getSpotifySong = song => {
 switch (action) {
 	case "spotify-this-song":
 		getSpotifySong(input);
+		break;
+	case "concert-this":
+		getVenueInfo(input);
 		break;
 }
